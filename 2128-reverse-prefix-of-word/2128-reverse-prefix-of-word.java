2695c0690f34eb31696f01544190b2645502d2a5
Gokilaselvi <gokilaselvgxlc@gmail.com>
2025-07-02 10:32:23 +0530

    Time: 5 ms (15.03%), Space: 42.2 MB (14.54%) - LeetHub

--- 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.java
+++ 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.java
@@ -3,16 +3,16 @@ class Solution {
         int len=word.length();
         int ind=0;
         String s="";
-        char[] arr=word.toCharArray();
+        //char[] arr=word.toCharArray();
         for(int i=0;i<len;i++){
-           if(ch==arr[i]){
+           if(ch==word.charAt(i)){
             ind=i;
             break;
            }
         }
         if(ind==0)return word;
         for(int i=ind;i>=0;i--){
-            s+=arr[i];
+            s+=word.charAt(i);
         }
         return s+word.substring(ind+1);
     }
