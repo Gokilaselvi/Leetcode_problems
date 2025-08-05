9036f29284d94c535a2aa08d076de098a469309b
Gokilaselvi <gokilaselvgxlc@gmail.com>
2025-08-05 13:55:47 +0530

    Time: 1 ms (100.00%) | Memory: 44.7 MB (22.68%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
@@ -1,15 +1,13 @@
 class Solution {
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
-     // Arrays.sort(fruits);
-     // Arrays.sort(baskets);
-      boolean [] barr=new boolean[fruits.length];
+     
       int count=0;
       for(int i=0;i<fruits.length;i++){
         boolean f=false;
         for(int j=0;j<baskets.length;j++){
-        if(!barr[j]&& fruits[i]<=baskets[j]){
-            barr[j]=true;
-            f=true;
+        if(fruits[i]<=baskets[j]){
+           baskets[j]=0;
+           f=true;
             break;
       }  }
       if(!f)count++;}
