04aed1ec2281ccfde306129f32c4339e3f7ed46f
Gokilaselvi <gokilaselvgxlc@gmail.com>
2025-08-06 19:54:50 +0530

    Time: 3 ms (68.99%) | Memory: 41.3 MB (69.20%) - LeetSync

--- 2824-check-if-the-number-is-fascinating/check-if-the-number-is-fascinating.java
+++ 2824-check-if-the-number-is-fascinating/check-if-the-number-is-fascinating.java
@@ -1,5 +1,19 @@
 class Solution {
     public boolean isFascinating(int n) {
-        return n == 192 || n == 219 || n==273 || n==327;
+        String  check="";
+        check+=String.valueOf((n));        
+        check+=String.valueOf((n*2));
+        check+=String.valueOf((n*3));
+        int []farr=new int[10];
+        //long num=Integer.parseInt(check);
+        for(char c:check.toCharArray()){
+            int rem=c-'0';
+            farr[rem]++;
+        }
+        for(int i=1;i<10;i++){
+            if(farr[i]!=1)return false;
+        }
+        
+ return true;
     }
 }
