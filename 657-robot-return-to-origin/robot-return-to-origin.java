4d4768b0223837b7ca720c3f9d455ecfeee3009a
Gokilaselvi <gokilaselvgxlc@gmail.com>
2025-09-05 07:48:26 +0530

    Time: 4 ms (96.90%) | Memory: 44.4 MB (81.92%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        //int [] farr=new int [26];
+        int u=0;
+        int d=0;
+        int l=0;
+        int r=0;
+        for(char c:moves.toCharArray()){
+            if(c=='U')u++;
+            else if(c=='D')d++;
+            else if(c=='L')l++;
+            else r++;
+        }
+        return (u==d)&&(l==r);
+    }
+}
