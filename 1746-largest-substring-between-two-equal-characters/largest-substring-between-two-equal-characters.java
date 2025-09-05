d42ac3d17b3150c58e1b167ccc4efc3f54c8a032
Gokilaselvi <gokilaselvgxlc@gmail.com>
2025-09-05 15:08:57 +0530

    Time: 1 ms (90.13%) | Memory: 41.8 MB (21.60%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.java
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.java
@@ -4,15 +4,13 @@ class Solution {
         int ans=-1;
         int indf=0;
         int indl=0;
-        String sub;
         char c;
         for(int i=0;i<len;i++){
            c=s.charAt(i);
            indf=s.indexOf(c);
            indl=s.lastIndexOf(c);
            if(indl!=indf){
-            sub=s.substring(indf+1,indl);
-           ans=Math.max(ans,sub.length());
+           ans=Math.max(ans,indl-indf-1);
            }  
             
             }
