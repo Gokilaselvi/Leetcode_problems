a70f61ad47e22aa99bc499694b2339fb6b6ce007
Gokilaselvi <gokilaselvgxlc@gmail.com>
2025-09-05 07:52:52 +0530

    Time: 2 ms (99.77%) | Memory: 44.7 MB (33.49%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -2,9 +2,9 @@ class Solution {
     public boolean judgeCircle(String moves) {
         int [] farr=new int [26];
         
-        char c;
-        for(int i=0;i<moves.length();i++){
-            c=moves.charAt(i);
+      //  char c;
+        for(char c:moves.toCharArray()){
+            //c=moves.charAt(i);
             farr[c-'A']++;
         }
         int u=farr[20];
