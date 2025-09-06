1eb494d8693997f1bf16156c26d861c29000aa3d
Gokilaselvi <gokilaselvgxlc@gmail.com>
2025-09-06 09:57:35 +0530

    Time: 2 ms (89.57%) | Memory: 45.7 MB (72.85%) - LeetSync

--- 1839-decode-xored-array/decode-xored-array.java
+++ 1839-decode-xored-array/decode-xored-array.java
@@ -3,11 +3,10 @@ class Solution {
         int len=encoded.length;
         int [] arr=new int [len+1];
         arr[0]=first;
-        //arr[1]=first^encoded[0];
-        int ind=1;
+        //int ind=1;
         for(int i=0;i<len;i++ ){
-            arr[ind]=encoded[i]^arr[ind-1];
-            ind++;
+            arr[i+1]=encoded[i]^arr[i];
+           
         }
         return arr;
     }
