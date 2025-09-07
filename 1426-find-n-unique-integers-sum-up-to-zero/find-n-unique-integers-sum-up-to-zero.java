85cb7cd89cb5acc2bf71d22d2fea491ef61f284a
Gokilaselvi <gokilaselvgxlc@gmail.com>
2025-09-07 22:07:10 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (95.19%) - LeetSync

---
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] sumZero(int n) {
+        int [] arr=new int[n];
+        int pos=0;
+        for(int i=1;i<=n/2;i++){
+           arr[pos++]=i;
+           arr[pos++]=-i;
+        }if(n%2==1)arr[pos]=0;
+        return arr;
+    }
+}
