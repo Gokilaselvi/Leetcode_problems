d784b37406e9b880e19c024618cfa94466e70c5f
Gokilaselvi <gokilaselvgxlc@gmail.com>
2025-12-14 14:43:36 +0530

    Time: 2 ms (100.00%) | Memory: 46.5 MB (39.76%) - LeetSync

---
+++ 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> intersection(int[][] nums) {
+        List<Integer>intersect=new ArrayList<>();
+        int row=nums.length;
+       
+        int [] freq=new int[1001];
+        
+        for(int i=0;i<row;i++){
+            for(int j=0;j<nums[i].length;j++){
+               // System.out.println(i+" "+j);
+               int num=nums[i][j];
+                freq[num]++;
+            }
+        }
+        for(int i=0;i<1001;i++){
+           
+            if(freq[i]==row)intersect.add(i);
+        }
+        return intersect;
+    }
+}
