853b63a023b757dbd1800b7ce359dfb7c2a9c35e
Gokilaselvi <gokilaselvgxlc@gmail.com>
2026-01-10 15:56:01 +0530

    Time: 25 ms (35.42%) | Memory: 45.3 MB (7.08%) - LeetSync

---
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minimumCost(int[] cost) {
+        Arrays.sort(cost);
+        int candiesCost=0;
+        int len=cost.length;
+        // if(len<3){
+        //     if(len==2){
+        //         candiesCost=cost[0]+cost[1];
+        //     }
+        //     else{
+        //         candiesCost+=cost[0];
+        //     }return candiesCost;
+        // }
+        int val=1;
+        for(int i=len-1;i>=0;i--){
+            if(val%3!=0){
+                System.out.println(cost[i]);
+                candiesCost+=cost[i];
+                val++;
+            }
+            else{
+                val=1;
+            }
+        }return candiesCost;
+    }
+}
