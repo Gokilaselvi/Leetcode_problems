9c9ee6785dc1ea318901a46a0b3da12a6c2381bb
Gokilaselvi <gokilaselvgxlc@gmail.com>
2026-01-10 16:11:40 +0530

    Time: 5 ms (96.81%) | Memory: 44.6 MB (55.83%) - LeetSync

--- 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.java
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.java
@@ -3,14 +3,7 @@ class Solution {
         Arrays.sort(cost);
         int candiesCost=0;
         int len=cost.length;
-        // if(len<3){
-        //     if(len==2){
-        //         candiesCost=cost[0]+cost[1];
-        //     }
-        //     else{
-        //         candiesCost+=cost[0];
-        //     }return candiesCost;
-        // }
+        
         int val=1;
         for(int i=len-1;i>=0;i--){
             if(val%3!=0){
